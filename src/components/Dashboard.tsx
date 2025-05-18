f878bced09f049101a83fbee0709a4ed841261b9
Elliot Rosen <elliotrosen2@gmail.com>
2025-05-18 15:53:14 -0400

    Add function names to fill in

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+
 const Dashboard = () => {
   const [instructionSets, setInstructionSets] = useState<InstructionSet[]>(() => {
     const saved = localStorage.getItem('instructionSets');
-    return saved ? JSON.parsed(saved) : [];
+    return saved ? JSON.parse(saved) : [];
   })
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [editingSet, setEditingSet] = useState<InstructionSet | null>(null);
+
+  useEffect(() => {
+    localStorage.setItem('instructionSets', JSON.stringify(instructionSets));
+  }, [instructionSets])
+
+  const handleAddSet = () => {};
+
+  const handleEditSet = () => {};
+
+  const handleDeleteSet = () => {};
+
+  const openCreateModal = () => {};
+
+  const openEditModal = () => {};
+
+  return (
+    <div></div>
+  )
 };
 
 export default Dashboard;
